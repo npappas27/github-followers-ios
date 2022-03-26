3b9f82f705286a2f4493ee6358401672744a7310
npappas27 <nickpappas@austins-mbp.lan>
2022-03-26 00:40:48 -0400

    user info refactor

--- GHFollowers/View Controllers/UserInfoViewController.swift
+++ GHFollowers/View Controllers/UserInfoViewController.swift
@@ -3,42 +3,83 @@ import UIKit
 class UserInfoViewController: UIViewController {
     
     let headerView = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
+    
+    var itemViews: [UIView] = []
     
     var username: String!
+    var user: User!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureVC()
+        layoutUI()
+        getUserInfo()
+    }
+    
+    func configureVC() {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        layoutUI()
-
-        
+    }
+    
+    func getUserInfo() {
         NetworkManager.shared.getUserInfo(username: username, completed: { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let user):
-                DispatchQueue.main.async {
+                self.user = user
+                DispatchQueue.main.async { [weak self] in
+                    guard let self = self else { return }
                     self.add(childVC: GFUserInfoHeaderViewController(user: user), to: self.headerView)
                 }
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Erorr", message: error.rawValue, buttonTitle: "OK")
             }
         })
+
     }
     
     func layoutUI() {
-        view.addSubview(headerView)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
+
+        itemViews = [headerView, itemViewOne, itemViewTwo]
+        itemViews.forEach { item in
+            view.addSubview(item)
+            item.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                item.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                item.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            ])
+        }
+        
         headerView.backgroundColor = .systemBackground
+        itemViewOne.backgroundColor = .systemPink
+        itemViewTwo.backgroundColor = .systemBlue
+                
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+            
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
+            
+            
         ])
-//        let header = GFUserInfoHeaderViewController(user: us)
     }
+    
+    // this func does what child vc does, seems more practical.
+//    func configureHeader(user: User) {
+//        let vc = GFUserInfoHeaderViewController(user: user)
+//        vc.view.frame = headerView.bounds
+//        self.headerView.addSubview(vc.view)
+//    }
     
     func add(childVC: UIViewController, to containerView: UIView) {
         addChild(childVC)
